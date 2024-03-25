37e9c3436cd494b5539301bb18f7c93392835357
Jere Mäennenä <jere.maennena@gmail.com>
2024-03-26 01:37:03 +0200

    Small fixes: Added second parameter to constructor and updated serial port handling

--- bank-automat/lib/DLL_rfid/dll_rfid.cpp
+++ bank-automat/lib/DLL_rfid/dll_rfid.cpp
@@ -2,15 +2,15 @@
 #include <QSerialPortInfo>
 #include <QDebug>
 
-DLL_rfid::DLL_rfid(QObject *parent)
-    : QObject(parent), serialport(new QSerialPort)
+DLL_rfid::DLL_rfid(QObject *parent, const QString &comPort)
+    : QObject(parent), serialport(new QSerialPort(comPort))
 {
-    serialport ->setPortName("COM3");                                       // asetetaan sarjaportin portti (tarkista omasi)
+    serialport->setPortName(comPort);                                       // asetetaan sarjaportti
     serialport->setBaudRate(QSerialPort::Baud19200);                        // asetetaan baudinopeus
     serialport->setDataBits(QSerialPort::Data8);                            // asetetaan datan bittikoko
     serialport->setParity(QSerialPort::NoParity);                           // asetetaan pariteetti
     serialport->setStopBits(QSerialPort::OneStop);                          // asetetaan stop-bittien määrä
-    
+
     connect(serialport, SIGNAL(readyRead()), this, SLOT(readData()));       // yhdistetään signaali ja slotti
 }
 
@@ -20,15 +20,28 @@ void DLL_rfid::openSerialPort()
     serialport->open(QIODevice::ReadWrite);
 }
 
+void DLL_rfid::transmitData(const QString& data)
+{
+    // lähetetään data sarjaportin kautta
+    serialport->write(data.toUtf8());
+}
+
 void DLL_rfid::returnCardID()
 {
     QByteArray cleanedData = data.mid(3, data.length() - 4).trimmed();      // poistetaan tarpeettomat etu- ja takamerkit ID-datasta
-    qDebug() << "DLL_rfid: Kortin siistitty ID data: " << cleanedData;      // debuggia
-    emit cardRead(cleanedData);                                             // lähetetään signaali siistitystä ID-datasta
+    qDebug() << "DLL_rfid: Kortin siistitty ID data: " << cleanedData;       // debuggia
+    emit cardRead(cleanedData);                                              // lähetetään signaali siistitystä ID-datasta
 }
 
 DLL_rfid::~DLL_rfid()
 {
     // poistetaan sarjaportti-olio
     delete serialport;
+}
+
+void DLL_rfid::readData()
+{
+    // luetaan dataa sarjaportilta
+    data = serialport->readAll();
+    returnCardID();
 }
