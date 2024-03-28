e70bf141a1715f243f9d389a7a04311be7159dba
Santeri Pasula <santeripasula1@gmail.com>
2024-03-28 16:59:34 +0200

    Käyttöliittymän pohja

--- bank-automat/mainwindow.cpp
+++ bank-automat/mainwindow.cpp
@@ -1,16 +1,15 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include "login.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    setWindowTitle("Tervetuloa");
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
