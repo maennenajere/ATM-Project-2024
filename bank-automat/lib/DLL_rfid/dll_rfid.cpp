d47050bd72b6b7cb4911b30fe66ac1e62e6015bb
Jere Mäennenä <jere.maennena@gmail.com>
2024-04-02 23:28:01 +0300

    Debug and small fixes

--- bank-automat/lib/DLL_rfid/dll_rfid.cpp
+++ bank-automat/lib/DLL_rfid/dll_rfid.cpp
@@ -6,12 +6,12 @@ DLL_rfid::DLL_rfid(QObject *parent, const QString &comPort)
     : QObject(parent), serialport(new QSerialPort(comPort))
 {
     serialport->setPortName(comPort);                                       // asetetaan sarjaportti
-    serialport->setBaudRate(QSerialPort::Baud19200);                        // asetetaan baudinopeus
+    serialport->setBaudRate(QSerialPort::Baud9600);                         // asetetaan baudinopeus
     serialport->setDataBits(QSerialPort::Data8);                            // asetetaan datan bittikoko
     serialport->setParity(QSerialPort::NoParity);                           // asetetaan pariteetti
     serialport->setStopBits(QSerialPort::OneStop);                          // asetetaan stop-bittien määrä
 
-    connect(serialport, SIGNAL(readyRead()), this, SLOT(readData()));       // yhdistetään signaali ja slotti
+    connect(serialport, &QSerialPort::readyRead, this, &DLL_rfid::readData);// yhdistetään signaali ja slotti
 }
 
 void DLL_rfid::openSerialPort()
@@ -28,9 +28,10 @@ void DLL_rfid::transmitData(const QString& data)
 
 void DLL_rfid::returnCardID()
 {
-    QByteArray cleanedData = data.mid(3, data.length() - 4).trimmed();      // poistetaan tarpeettomat etu- ja takamerkit ID-datasta
-    qDebug() << "DLL_rfid: Kortin siistitty ID data: " << cleanedData;       // debuggia
-    emit cardRead(cleanedData);                                              // lähetetään signaali siistitystä ID-datasta
+    QByteArray cleanedData = data.trimmed();
+    cleanedData = cleanedData.mid(1, cleanedData.length() - 3);              // poistetaan tarpeettomat etu- ja takamerkit
+    qDebug() << "DLL_rfid: Kortin siistitty ID data: " << cleanedData;       // debug
+    emit cardRead(cleanedData);                                              // signaali siistitystä ID-datasta
 }
 
 DLL_rfid::~DLL_rfid()
@@ -43,5 +44,7 @@ void DLL_rfid::readData()
 {
     // luetaan dataa sarjaportilta
     data = serialport->readAll();
+    qDebug() << "DLL_rfid: Vastaanotettu data (hex): " << data.toHex();      // debug
+    qDebug() << "DLL_rfid: Vastaanotettu data: " << data;                    // debug
     returnCardID();
 }
--- bank-automat/lib/DLL_rfid/dll_rfid.h
+++ bank-automat/lib/DLL_rfid/dll_rfid.h
@@ -14,20 +14,20 @@ class DLL_RFID_EXPORT DLL_rfid : public QObject
 public:
     DLL_rfid(QObject *parent, const QString &comPort);     // DLL_rfid konstruktori
     ~DLL_rfid();                                            // DLL_rfid destruktori
+    QSerialPort * serialport;                               // luodaan olio serialportille
 
     void openSerialPort();                                  // avataan sarjaportti
     void transmitData(const QString& data);                 // metodi datan lähettämiseksi
     void returnCardID();                                    // metodi kortin ID:n palauttamiseksi
 
 signals:
-    void cardRead(QByteArray data);                         // signaali kortin lukemisen ilmoittamiseksi
+    void cardRead(const QByteArray &data);                  // signaali kortin lukemisen ilmoittamiseksi
     void readyRead();                                       // signaali sarjaportin lukemisen valmiudesta
 
 private slots:
     void readData();
 
 private:
-    QSerialPort * serialport;                               // luodaan olio serialportille
     QByteArray data;                                        // muuttuja datan tallentamiseksi
 };
 
